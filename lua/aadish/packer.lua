1c9e2bedd1e1e613fda631f599dd5f863490dc03
Aadishx07 <aadishdas7@gmail.com>
2024-03-17 23:32:59 +0530

    add

--- lua/aadish/packer.lua
+++ lua/aadish/packer.lua
@@ -1,29 +1,19 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Begin Packer bootstrap
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({
-        'git',
-        'clone',
-        '--depth', '1',
-        'https://github.com/wbthomason/packer.nvim',
-        install_path
-    })
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
--- End Packer bootstrap
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
-    -- Your plugins here
+    -- My plugins here
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
     use {
@@ -43,10 +33,17 @@ return require('packer').startup(function(use)
     }
     use 'mbbill/undotree'
     use 'navarasu/onedark.nvim'
+    use {'akinsho/bufferline.nvim', tag = "*"}
     use 'nvim-tree/nvim-tree.lua'
     use 'lukas-reineke/indent-blankline.nvim'
     use 'nvim-tree/nvim-web-devicons'
     use 'lewis6991/gitsigns.nvim'
+    -- LSP configurations start
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
@@ -61,15 +58,8 @@ return require('packer').startup(function(use)
             {'onsails/lspkind.nvim'},
         }
     }
-    use {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    }
+    -- LSP configurations end
 
-
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
     if packer_bootstrap then
         require('packer').sync()
     end
