26e8c778dc2ce4f3a983195d737de87ffbe7de6f
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-03-27 13:41:15 +0530

    add

--- lua/aadish/packer.lua
+++ lua/aadish/packer.lua
@@ -1,54 +1,85 @@
+-- Ensure Packer is installed
 local ensure_packer = function()
     local fn = vim.fn
+    -- Define the install path for Packer
     local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    -- Check if Packer is installed; if not, clone Packer from GitHub into the correct directory
     if fn.empty(fn.glob(install_path)) > 0 then
         fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
         vim.cmd [[packadd packer.nvim]]
-        return true
+        return true -- Return true to indicate that Packer was not previously installed
     end
-    return false
+    return false -- Return false to indicate that Packer was already installed
 end
 
+-- Attempt to ensure Packer is installed, and store the result
 local packer_bootstrap = ensure_packer()
 
+-- Packer configuration
 return require('packer').startup(function(use)
+    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-    -- My plugins here
+
+    -- Plugin definitions
+    -- nvim-treesitter for advanced syntax highlighting and more
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
+    -- lualine for a fancy status line
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+
+    -- telescope for fuzzy finding and more
     use {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
+
+    -- fidget for LSP progress
     use {
         "j-hui/fidget.nvim",
         config = function()
             require("fidget").setup({})
         end
     }
+
+    -- undotree for visualizing vim undo history
     use 'mbbill/undotree'
+
+    -- onedark theme for Neovim
     use 'navarasu/onedark.nvim'
+
+    -- bufferline for managing buffers
     use { 'akinsho/bufferline.nvim', tag = "*" }
+
+    -- Required for various file icons
     use 'nvim-tree/nvim-web-devicons'
+
+    -- nvim-tree for file explorer
     use 'nvim-tree/nvim-tree.lua'
+
+    -- indent-blankline for visually displaying indents
     use 'lukas-reineke/indent-blankline.nvim'
+
+    -- gitsigns for git integration
     use 'lewis6991/gitsigns.nvim'
-    -- LSP configurations start
+
+    -- LSP (Language Server Protocol) configurations start
+    -- Mason for managing LSP servers, linters, and formatters
     use {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
     }
+
+    -- lsp-zero for a simplified LSP setup
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         requires = {
-            -- Autocompletion
+            -- Various plugins for autocompletion
             { 'hrsh7th/nvim-cmp' },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'honza/vim-snippets' },
@@ -60,7 +91,10 @@ return require('packer').startup(function(use)
     }
     -- LSP configurations end
 
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- so Neovim doesn't have to be restarted
     if packer_bootstrap then
         require('packer').sync()
     end
 end)
+
--- lua/aadish/set.lua
+++ lua/aadish/set.lua
@@ -1,14 +1,19 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.nu = true  -- Enable line numbers
+vim.opt.relativenumber = true  -- Display line numbers as relative to the line with the cursor
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.tabstop = 4  -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 4  -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.shiftwidth = 4  -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true  -- Convert tabs to spaces
 
-vim.opt.smartindent = true
+vim.opt.smartindent = true  -- Automatically insert indents in a smart way
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.opt.swapfile = false  -- Do not use a swapfile for the buffer
+vim.opt.backup = false  -- Do not keep a backup file after saving
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"  -- Directory where undo files are stored
+vim.opt.undofile = true  -- Save undo history to an undo file
+
+vim.opt.cursorline = true  -- Highlight the current line
+vim.opt.showcmd = true  -- Show command in the last line of the screen
+vim.opt.showmode = false  -- Do not display the mode (e.g., --INSERT--) in the command line area
+
