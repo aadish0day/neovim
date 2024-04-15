914b721ac2cb8d1b6a3801cca8231f4fcb67e6f8
Aadishx07 <aadishdas7@gmail.com>
2024-04-15 21:51:41 +0530

    add
    some improvement in treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,17 +1,27 @@
-require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all". Including Bash now.
+require('nvim-treesitter.configs').setup {
+    -- A list of parser names, or "all"
     ensure_installed = { "c", "lua", "html", "java", "javascript", "bash" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
 
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+
+    -- Enable indentation based on Treesitter
+    indent = {
+        enable = true
+    },
+
+    -- Add `modules` as an empty table if not used
+    modules = {},
+
+    -- Specify parsers to ignore during installation (empty if none)
+    ignore_install = {}
 }
 
