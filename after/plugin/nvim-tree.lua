3ece03cce5a432f6783333a45f23863f6ea20d2f
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-05-06 19:17:50 +0530

    add

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -17,6 +17,32 @@ require("nvim-tree").setup({
     },
     view = {
         width = 25,
+        float = {
+            enable = true,
+            quit_on_focus_loss = true,
+            open_win_config = function()
+                -- Constants for window dimensions ratios
+                local WIDTH_RATIO = 0.8  -- You can adjust this value
+                local HEIGHT_RATIO = 0.8  -- You can adjust this value
+
+                local screen_w = vim.opt.columns:get()
+                local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                local window_w = screen_w * WIDTH_RATIO
+                local window_h = screen_h * HEIGHT_RATIO
+                local window_w_int = math.floor(window_w)
+                local window_h_int = math.floor(window_h)
+                local center_x = (screen_w - window_w) / 2
+                local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+                return {
+                    border = "rounded",
+                    relative = "editor",
+                    row = center_y,
+                    col = center_x,
+                    width = window_w_int,
+                    height = window_h_int,
+                }
+            end,
+        },
     },
     renderer = {
         group_empty = false,
@@ -60,3 +86,4 @@ require("nvim-tree").setup({
         dotfiles = false,
     },
 })
+
