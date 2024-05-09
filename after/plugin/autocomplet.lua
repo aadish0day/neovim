26e270a1e90cb8bde0bda26cff5a697dd40dba43
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-05-09 20:46:27 +0530

    add

--- after/plugin/autocomplet.lua
+++ after/plugin/autocomplet.lua
@@ -1,7 +1,33 @@
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 
-require("luasnip.loaders.from_vscode").lazy_load()
+-- Loads VSCode-format snippets, excluding certain snippets if specified in the Neovim configuration
+require("luasnip.loaders.from_vscode").lazy_load { exclude = vim.g.vscode_snippets_exclude or {} }
+
+-- Loads VSCode-format snippets from a specified path
+require("luasnip.loaders.from_vscode").lazy_load { paths = "your path!" }
+require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+
+-- Loads SnipMate-format snippets using default paths
+require("luasnip.loaders.from_snipmate").load()
+
+-- Loads SnipMate-format snippets from specified paths lazily
+require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+
+-- Loads Lua-format snippets using default paths
+require("luasnip.loaders.from_lua").load()
+
+-- Lazily loads Lua-format snippets from specified paths
+require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+        if require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()] and not require("luasnip").session.jump_active then
+            require("luasnip").unlink_current()
+        end
+    end,
+})
 
 local function border(hl_name)
     return {
