3fb182231bc2dc13741dc843247ae6f54426f429
Aadishx07 <aadishdas7@gmail.com>
2024-08-28 19:26:30 +0530

    adding some feature

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -15,8 +15,8 @@ require('gitsigns').setup {
         follow_files = true
     },
     auto_attach = true,
-    attach_to_untracked = false,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    attach_to_untracked = true,
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
@@ -24,14 +24,14 @@ require('gitsigns').setup {
         ignore_whitespace = false,
         virt_text_priority = 100,
     },
-    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+    current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> - <summary>',
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil,  -- Use default
     max_file_length = 40000, -- Disable if file is longer than this (in lines)
     preview_config = {
         -- Options passed to nvim_open_win
-        border = 'single',
+        border = 'rounded',
         style = 'minimal',
         relative = 'cursor',
         row = 0,
