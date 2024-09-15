1bda869a0639b1e7c70e647599641bf9df6865f4
Aadishx07 <aadishdas7@gmail.com>
2024-09-15 23:16:09 +0530

    Adding nvim-cmp to autopairs

--- lua/aadish/lazy/autopairs.lua
+++ lua/aadish/lazy/autopairs.lua
@@ -1,10 +1,20 @@
-return
-{
+-- ~/.config/nvim/lua/aadish/lazy/autocomplet.lua
+
+return {
     {
         'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        config = true
-        -- use opts = {} for passing setup options
-        -- this is equivalent to setup({}) function
+        event = "InsertEnter",  -- Load the plugin when entering Insert mode
+        config = function()
+            require('nvim-autopairs').setup({
+                disable_filetype = { "TelescopePrompt" , "vim" },
+                check_ts = true,  -- Enable treesitter integration
+            })
+
+            -- If using nvim-cmp for autocompletion, set up completion pairing
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+            cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+        end
     },
 }
+
