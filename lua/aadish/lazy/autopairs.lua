195e91ae9a7ed2f443a44f0287f825a33900d1b0
Aadishx07 <aadishdas7@gmail.com>
2024-09-19 23:04:04 +0530

    Adding autopairs to work with LSP

--- lua/aadish/lazy/autopairs.lua
+++ lua/aadish/lazy/autopairs.lua
@@ -1,20 +1,18 @@
--- ~/.config/nvim/lua/aadish/lazy/autocomplet.lua
-
 return {
     {
         'windwp/nvim-autopairs',
-        event = "InsertEnter",  -- Load the plugin when entering Insert mode
+        event = "InsertEnter", -- Load the plugin when entering Insert mode
         config = function()
             require('nvim-autopairs').setup({
-                disable_filetype = { "TelescopePrompt" , "vim" },
-                check_ts = true,  -- Enable treesitter integration
+                disable_filetype = { "TelescopePrompt", "vim" },
+                check_ts = true, -- Enable treesitter integration
             })
 
             -- If using nvim-cmp for autocompletion, set up completion pairing
             local cmp_autopairs = require('nvim-autopairs.completion.cmp')
             local cmp = require('cmp')
             cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+            -- loadstring
         end
     },
 }
-
