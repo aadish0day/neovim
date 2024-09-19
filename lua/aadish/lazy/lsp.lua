8739d2b36fe0bd7566567737e85ba2522684a8a7
Aadishx07 <aadishdas7@gmail.com>
2024-09-19 23:17:29 +0530

    Auto update the lsp

--- lua/aadish/lazy/lsp.lua
+++ lua/aadish/lazy/lsp.lua
@@ -31,7 +31,7 @@ return {
             -- Set up mason-tool-installer
             require("mason-tool-installer").setup({
                 ensure_installed = { 'shfmt' },
-                auto_update = false,
+                auto_update = true,
                 run_on_start = true,
                 start_delay = 3000, -- 3 second delay
                 debounce_hours = 5, -- at least 5 hours between attempts to install/update
