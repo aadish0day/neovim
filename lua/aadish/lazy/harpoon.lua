75e57fcb32dd60bc219d52574319c70da5aada17
Aadishx07 <aadishdas7@gmail.com>
2024-09-21 01:48:36 +0530

    Adding harpoon

---
+++ lua/aadish/lazy/harpoon.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        'ThePrimeagen/harpoon',
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            -- Ensure Harpoon is properly set up
+            require("harpoon").setup()
+
+            -- Key mappings
+            vim.api.nvim_set_keymap('n', '<leader>a', ':lua require("harpoon.mark").add_file()<CR>',
+                { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>h', ':lua require("harpoon.ui").toggle_quick_menu()<CR>',
+                { noremap = true, silent = true })
+
+            -- Navigate to marked files
+            vim.api.nvim_set_keymap('n', '<leader>1', ':lua require("harpoon.ui").nav_file(1)<CR>',
+                { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>2', ':lua require("harpoon.ui").nav_file(2)<CR>',
+                { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>3', ':lua require("harpoon.ui").nav_file(3)<CR>',
+                { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>4', ':lua require("harpoon.ui").nav_file(4)<CR>',
+                { noremap = true, silent = true })
+        end
+    }
+}
