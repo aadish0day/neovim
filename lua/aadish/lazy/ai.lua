71a7723d0673d10dad0d1f9faf38974acaf08ee1
Aadishx07 <aadishdas7@gmail.com>
2024-09-22 02:39:46 +0530

    Feat(ai): adding codeium

--- lua/aadish/lazy/ai.lua
+++ lua/aadish/lazy/ai.lua
@@ -32,17 +32,12 @@ return {
         end,
 
     },
-    -- Remove the `use` here if you're using folke/lazy.nvim.
     {
         'Exafunction/codeium.vim',
         config = function()
-            -- Change '<C-g>' here to any keycode you like.
-            vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-            vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
-                { expr = true, silent = true })
-            vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-                { expr = true, silent = true })
             vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+
+            vim.g.codeium_enabled = false
         end
-    }
+    },
 }
