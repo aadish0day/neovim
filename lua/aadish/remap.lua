b7bdb4fa32cd57e05f1b4f68c72813ea62b16505
Aadishx07 <aadishdas7@gmail.com>
2024-09-21 15:04:15 +0530

    remove the remap to open :Ex

--- lua/aadish/remap.lua
+++ lua/aadish/remap.lua
@@ -1,9 +1,6 @@
 -- Neovim Configurations
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.g.mapleader = " "
-
 -- Moving lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
