81bc91625175b1243c6388dc408c941f6495aaf1
Aadishx07 <aadishdas7@gmail.com>
2024-09-22 02:38:14 +0530

    feat(treesitter): Update language parsers and configuration

--- lua/aadish/lazy/treesitter.lua
+++ lua/aadish/lazy/treesitter.lua
@@ -1,32 +1,34 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require('nvim-treesitter.configs').setup {
-            -- A list of parser names, or "all"
-            ensure_installed = { "c", "lua", "html", "java", "javascript", "bash" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                -- A list of parser names, or "all"
+                ensure_installed = { "c", "lua", "html", "java", "javascript", "bash" },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = true,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = true,
 
-            -- Automatically install missing parsers when entering buffer
-            auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                auto_install = true,
 
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
 
-            -- Enable indentation based on Treesitter
-            indent = {
-                enable = true
-            },
+                -- Enable indentation based on Treesitter
+                indent = {
+                    enable = true
+                },
 
-            -- Add `modules` as an empty table if not used
-            modules = {},
+                -- Add `modules` as an empty table if not used
+                modules = {},
 
-            -- Specify parsers to ignore during installation (empty if none)
-            ignore_install = {}
-        }
-    end
+                -- Specify parsers to ignore during installation (empty if none)
+                ignore_install = {}
+            }
+        end
+    },
 }
