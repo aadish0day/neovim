39cbda4ed9e2f250373f52684f0d8f3c6d756dc2
Aadishx07 <aadishdas7@gamil.com>
2024-10-04 22:09:53 +0530

    add none-ls

--- lua/aadish/lazy/none-ls.lua
+++ lua/aadish/lazy/none-ls.lua
@@ -1,15 +1,15 @@
 -- Plugin setup (assuming you're using packer.nvim)
 return {
 	{
-"nvimtools/none-ls.nvim",
-        config = function()
-                        require("null-ls").setup({
-                sources = {
-                    require("null-ls").builtins.formatting.shfmt,
-                    require("null-ls").builtins.formatting.clang_format,
-                    require("null-ls").builtins.formatting.stylua,
-                },
-            })
+		"nvimtools/none-ls.nvim",
+		config = function()
+			require("null-ls").setup({
+				sources = {
+					require("null-ls").builtins.formatting.shfmt,
+					require("null-ls").builtins.formatting.clang_format,
+					require("null-ls").builtins.formatting.stylua,
+				},
+			})
 		end,
 	},
 }
