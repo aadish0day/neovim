c01a3f1b649a9e95d6fd61f9d4ef358243ed0c4d
Aadishx07 <aadishdas7@gamil.com>
2024-10-05 20:46:36 +0530

    feat(lsp): mason-null-ls

--- lua/aadish/lazy/lsp.lua
+++ lua/aadish/lazy/lsp.lua
@@ -4,7 +4,7 @@ return {
 		dependencies = {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			"jay-babu/mason-null-ls.nvim",
 			"hrsh7th/nvim-cmp",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
@@ -26,65 +26,34 @@ return {
 					},
 				},
 			})
-			vim.keymap.set("n", "<leader>m", ":Mason<CR>", { silent = true })
 
-			-- Mason-tool-installer setup
-			require("mason-tool-installer").setup({
+			-- Mason LSP setup
+			require("mason-lspconfig").setup({
 				ensure_installed = {
-					"shfmt",
 					"lua_ls",
-					"html",
-					"cssls",
-					"jdtls",
 					"bashls",
 					"clangd",
+					"cssls",
+					"html",
+					"jdtls",
 					"ts_ls",
-					"stylua",
 				},
-				auto_update = true,
-				run_on_start = true,
-				start_delay = 3000,
-				debounce_hours = 5,
+				automatic_installation = true,
 			})
 
-			require("fidget").setup({
-				text = {
-					spinner = "dots", -- You can choose from 'dots', 'line', 'dots_pulse', 'line_pulse', 'moon', etc.
-					done = "✔️", -- Character displayed when the task is done
-					commenced = "Started", -- Message displayed when a task starts
-					completed = "Completed", -- Message displayed when a task completes
+			-- Mason Null LS setup
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"shfmt",
+					"stylua",
 				},
-				align = {
-					bottom = true, -- Align the fidget UI to the bottom of the window
-					right = true, -- Align the fidget UI to the right of the window
-				},
-				timer = {
-					spinner_rate = 125, -- How fast the spinner updates (in milliseconds)
-					fidget_decay = 2000, -- Time in milliseconds to wait before fading out the fidget
-				},
-				window = {
-					blend = 0, -- Transparency level of the fidget window
-				},
-				fmt = {
-					leftpad = true, -- Pad the messages on the left
-					max_width = 30, -- Set a maximum width for the messages
-				},
-				--- Controls when fidget should show
-				-- These are all optional
-				-- 'LspAttach' will show fidget when LSP attaches to a buffer
-				-- 'LspProgress' will show fidget during LSP operations
-				-- 'InsertEnter' will show fidget when entering insert mode
-				-- 'InsertLeave' will hide fidget when leaving insert mode
-				-- 'DiagnosticChanged' will show fidget when diagnostics change
-				-- These can be set to true or false
-				show = {
-					LspAttach = true,
-					LspProgress = true,
-					InsertEnter = false,
-					InsertLeave = true,
-					DiagnosticChanged = true,
-				},
+				automatic_installation = true,
 			})
+
+			-- Keymapping to open Mason
+			vim.keymap.set("n", "<leader>m", ":Mason<CR>", { silent = true })
+
+			require("fidget").setup({})
 
 			-- LSP setup
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -104,7 +73,7 @@ return {
 			end
 
 			local lspconfig = require("lspconfig")
-			local servers = { "html", "cssls", "jdtls", "bashls", "clangd", "ts_ls" }
+			local servers = { "html", "cssls", "ts_ls", "bashls", "clangd" }
 
 			for _, server in ipairs(servers) do
 				lspconfig[server].setup({
@@ -113,24 +82,6 @@ return {
 				})
 			end
 
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-						runtime = { version = "LuaJIT" },
-						workspace = {
-							library = {
-								vim.fn.expand("$VIMRUNTIME/lua"),
-								vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-							},
-						},
-						telemetry = { enable = false },
-					},
-				},
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-
 			-- Snippet and completion setup
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
@@ -138,17 +89,6 @@ return {
 			require("luasnip.loaders.from_vscode").lazy_load()
 			require("luasnip.loaders.from_snipmate").load()
 			require("luasnip.loaders.from_lua").load()
-
-			vim.api.nvim_create_autocmd("InsertLeave", {
-				callback = function()
-					if
-						luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
-						and not luasnip.session.jump_active
-					then
-						luasnip.unlink_current()
-					end
-				end,
-			})
 
 			local kind_icons = {
 				Text = "",
@@ -185,7 +125,7 @@ return {
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
@@ -214,24 +154,11 @@ return {
 					{ name = "buffer" },
 					{ name = "path" },
 				}),
-				formatting = {
-					format = function(entry, vim_item)
-						vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
-						vim_item.menu = ({
-							nvim_lsp = "[LSP]",
-							luasnip = "[Snippet]",
-							buffer = "[Buffer]",
-							path = "[Path]",
-						})[entry.source.name]
-						return vim_item
-					end,
-				},
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
 			})
-
 			cmp.setup.cmdline(":", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
