5a19f0f04e2e3bc56ac3be68846f12f16baeff1e
Aadishx07 <aadishdas7@gmail.com>
2024-10-13 13:57:28 +0530

    feat(treesitter): add git

--- lua/aadish/lazy/treesitter.lua
+++ lua/aadish/lazy/treesitter.lua
@@ -1,36 +1,47 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				-- A list of parser names, or "all"
-				ensure_installed = { "c", "lua", "html", "java", "javascript", "bash" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all"
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "html",
+                    "java",
+                    "javascript",
+                    "bash",
+                    "gitcommit",
+                    "gitrebase",
+                    "diff",
+                    "markdown",
+                },
 
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-				-- Automatically install missing parsers when entering buffer
-				auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                auto_install = true,
 
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
 
-				-- Enable indentation based on Treesitter
-				indent = {
-					enable = true,
-				},
+                -- Enable indentation based on Treesitter
+                indent = {
+                    enable = true,
+                },
 
-				-- Add `modules` as an empty table if not used
-				modules = {},
+                -- Add `modules` as an empty table if not used
+                modules = {},
 
-				-- Specify parsers to ignore during installation (empty if none)
-				ignore_install = {},
-			})
-			-- Silence Treesitter messages on the first boot
-			vim.g.treesitter_silence_on_first_boot = true
-		end,
-	},
+                -- Specify parsers to ignore during installation (empty if none)
+                ignore_install = {},
+            })
+            -- Silence Treesitter messages on the first boot
+            vim.g.treesitter_silence_on_first_boot = true
+        end,
+    },
 }
