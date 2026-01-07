3f3f2e065e361b37689558fce4d912aeb196ccae
Aadishx07 <aadishdas7@gmail.com>
2026-01-07 20:47:27 +0530

    fix(telescope, treesitter): fix somethings

--- lua/aadish/lazy/telescope.lua
+++ lua/aadish/lazy/telescope.lua
@@ -1,70 +1,66 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope-ui-select.nvim", -- Added ui-select
-        },
-        config = function()
-            -- Require and configure Telescope
-            require("telescope").setup({
-                defaults = {
-                    -- Default configuration for Telescope goes here
-                    mappings = {
-                        i = {
-                            ["<C-h>"] = "which_key",
-                            ["<C-k>"] = require("telescope.actions").move_selection_previous, -- Move to previous result
-                            ["<C-j>"] = require("telescope.actions").move_selection_next, -- Move to next result
-                        },
-                    },
-                },
-                pickers = {
-                    -- Default configuration for builtin pickers goes here
-                    buffers = {
-                        sort_mru = true, -- Most recently used first
-                        ignore_current_buffer = true, -- Optional: hide current buffer
-                    },
-                },
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({
-                            -- Additional options for ui-select
-                        }),
-                    },
-                    fzf = {
-                        fuzzy = true, -- false will only do exact matching
-                        override_generic_sorter = true, -- override the generic sorter
-                        override_file_sorter = true, -- override the file sorter
-                        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-                        -- the default case_mode is "smart_case"
-                    },
-                },
-            })
-
-            -- Load Telescope extensions
-            require("telescope").load_extension("ui-select")
-            require("telescope").load_extension("fzf")
-
-            -- Key mappings for Telescope
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-            vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-            vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "Fuzzy find buffers" })
-
-            vim.keymap.set("n", "<leader>/", function()
-                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-                    winblend = 10,
-                    previewer = false,
-                }))
-            end, { desc = "Fuzzy find in current buffer" })
-        end,
-    },
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+		},
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					-- Disable treesitter in previews to fix ft_to_lang error
+					preview = {
+						treesitter = false,
+					},
+					mappings = {
+						i = {
+							["<C-h>"] = "which_key",
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+						},
+					},
+				},
+				pickers = {
+					buffers = {
+						sort_mru = true,
+						ignore_current_buffer = true,
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
+			})
+			
+			-- Load extensions
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("fzf")
+			
+			-- Keymaps
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Fuzzy find buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+			vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Git files" })
+			vim.keymap.set("n", "<leader>/", function()
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "Fuzzy find in current buffer" })
+		end,
+	},
 }
--- lua/aadish/lazy/treesitter.lua
+++ lua/aadish/lazy/treesitter.lua
@@ -1,46 +1,29 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                -- A list of parser names, or "all"
-                ensure_installed = {
-                    "c",
-                    "lua",
-                    "html",
-                    "java",
-                    "javascript",
-                    "bash",
-                    "gitcommit",
-                    "diff",
-                    "markdown",
-                },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false, -- Plugin doesn't support lazy-loading
+		build = ":TSUpdate",
+		config = function()
+			-- Install parsers (runs asynchronously)
+			require("nvim-treesitter").install({
+				"c",
+				"lua",
+				"html",
+				"java",
+				"javascript",
+				"bash",
+				"gitcommit",
+				"diff",
+				"markdown",
+			})
 
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
-
-                -- Automatically install missing parsers when entering buffer
-                auto_install = true,
-
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-
-                -- Enable indentation based on Treesitter
-                indent = {
-                    enable = true,
-                },
-
-                -- Add `modules` as an empty table if not used
-                modules = {},
-
-                -- Specify parsers to ignore during installation (empty if none)
-                ignore_install = {},
-            })
-            -- Silence Treesitter messages on the first boot
-            vim.g.treesitter_silence_on_first_boot = true
-        end,
-    },
+			-- Enable highlighting via autocmd
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "c", "lua", "html", "java", "javascript", "bash", "gitcommit", "diff", "markdown" },
+				callback = function()
+					vim.treesitter.start()
+				end,
+			})
+		end,
+	},
 }
